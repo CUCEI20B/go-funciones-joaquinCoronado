7f96a11d9a9d479e6a0d94f24e3ebb865aac4cc1
Joaquin Coronado <joaquincora@hotmail.com>
2020-10-16 00:43:54 -0500

    firs commit

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,21 @@
 package main
 
-func Burbuja(s []int64)  {
+func Burbuja(s []int64) {
+	vueltas := cap(s) - 1
+	aux := int64(0)
 
+	for i := 0; i < vueltas; i++ {
+		for j := 0; j < vueltas; j++ {
+			if s[j] > s[j+1] {
+				aux = s[j]
+				s[j] = s[j+1]
+				s[j+1] = aux
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+	slide := []int64{5, 4, 3, 2, 1}
+	Burbuja(slide)
 }
